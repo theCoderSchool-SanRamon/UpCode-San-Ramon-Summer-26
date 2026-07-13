a359ff8bbc6daf45cb81958cec5235575cd27bff
PrismaticDepths <109403545+PrismaticDepths@users.noreply.github.com>
2026-07-13 13:02:20 -0700

    install vue, vite, backend changes

--- src/backend/api_query.py
+++ src/backend/api_query.py
@@ -12,45 +12,17 @@ class APIRequester():
 		return requests.get(self.url, params=params)
 
 
-class CensusRequester():
+class CensusRequester(APIRequester):
 	def __init__(self):
 		try:
 			with open(".census_key", "r") as f:
-				key = f.read()
+				self.key = f.read()
+			super().__init__(self, "https://api.census.gov/data/2024/acs/acs5")
 		except FileNotFoundError:
-			input("Census API Key not found. Please provide at .census_key.")
-
-
-
-def query_place(place, state: str, get):
-	url = "https://api.census.gov/data/2024/acs/acs5"
-	params = {
-		"get": get,  # "NAME,B01001_001E"
-		"for": f'place:{place.rjust(5,"0")}',
-		"in": f'state:{state.rjust(2,"0")}',
-		"key": key,
-	}
-	if (place, state, get) in cache:
-		return cache[(place, state, get)]
-	else:
-		response = requests.get(url, params=params)
-		data = response.json()
-		cache[(place, state, get)] = data
-		return data
-
-
-def query_county(county, state: str, get):
-	url = "https://api.census.gov/data/2024/acs/acs5"
-	params = {
-		"get": get,  # "NAME,B01001_001E"
-		"for": f'county:{county.rjust(3,"0")}',
-		"in": f'state:{state.rjust(2,"0")}',
-		"key": key,
-	}
-	if (county, state, get) in cache:
-		return cache[(county, state, get)]
-	else:
-		response = requests.get(url, params=params)
-		data = response.json()
-		cache[(county, state, get)] = data
-		return data
+			print("Census API Key not found. Please provide at .census_key.")
+	def send_query(self, params: dict):
+		if "place" in params: params["for"] = params.pop("place").rjust(5,"0")
+		if "county" in params: params["for"] = params.pop("county").rjust(3,"0")
+		if "state" in params: params["in"] = params.pop("state").rjust(2,"0")
+		params["key"] = self.key
+		return super().send_query(self, params)
